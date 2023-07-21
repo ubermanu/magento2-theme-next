e596be45838f67f609bcb94ad9df5e56ab1ac69f
ubermanu <manu@paca.happy-dev.fr>
2023-07-21 11:03:44 +0200

    disable _keyfocus in favor of focus-visible

---
+++ web/mage/smart-keyboard-handler.js
@@ -0,0 +1,14 @@
+/**
+ * Disable the smart keyboard handler, in favor of the browser's native
+ * :focus-visible pseudo-class.
+ */
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    return {
+        apply: $.noop,
+        focus: $.noop,
+    };
+});
