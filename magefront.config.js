c376afbb33c8b2191af2eaba68e1532b05031000
ubermanu <manu@paca.happy-dev.fr>
2023-08-27 14:37:33 +0200

    fix config

--- magefront.config.js
+++ magefront.config.js
@@ -1,12 +1,14 @@
 import less4 from 'less';
-import less from 'magefront-plugin-less';
 
 export default {
     plugins: [
-        less({
-            compiler: less4,
-        }),
-        'magefront-plugin-requirejs',
-        'magefront-plugin-js-translations',
+        [
+            'magefront-plugin-less',
+            {
+                compiler: less4,
+            },
+        ],
+        'magefront-plugin-requirejs-config',
+        'magefront-plugin-js-translation',
     ],
 };
